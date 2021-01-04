649be7c8ea8b44b70fcc373286e66094d1d17f05
Feriel28 <49943552+Feriel28@users.noreply.github.com>
2021-01-04 21:56:53 +0100

    Add files via upload

---
+++ Help.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Help.h
+ * Author: Feriel
+ *
+ * Created on 13 décembre 2020, 12:41
+ */
+
+#ifndef HELP_H
+#define HELP_H
+
+#include <iostream>
+#include <string>
+
+
+class Help
+{
+public :
+    static void display();
+};
+
+#endif /* HELP_H */
+
